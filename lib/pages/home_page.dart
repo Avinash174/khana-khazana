adc58bb25857c77eeba4a2fa5e1aa218ee97242c
Avinash174 <avinashmagar15@gmail.com>
2023-12-07 19:46:38 +0530

    new changes done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,7 +11,9 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Container(
+        child: Text('Hello, Avinash Magar'),
+      ),
     );
   }
 }
